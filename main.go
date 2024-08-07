46e47dc5de1d986ea613e117fcb161cc288aea40
Brian <brian.jincheng.yu@outlook.com>
2024-08-07 18:03:01 +0800

    For running [dev v0.0.1 brian_builder]Setup the env and write the prefetcher test function

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus/ethash"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/ethdb"
+)
+
+// 读取disk中的数据库并建立区块链
+func GetBlockChain() (ethdb.Database, *core.BlockChain) {
+	datadir := "/home/user/common/docker/volumes/eth-docker_geth-eth1-data/_data/geth/chaindata"
+	// datadir := "/home/user/data/ben/cp1_eth-docker_geth-eth1-data/_data/geth/chaindata"
+	ancient := datadir + "/ancient"
+	db, err := rawdb.Open(
+		rawdb.OpenOptions{
+			Directory:         datadir,
+			AncientsDirectory: ancient,
+			Ephemeral:         true,
+		},
+	)
+	if err != nil {
+		fmt.Println("rawdb.Open err!", err)
+	}
+	bc, _ := core.NewBlockChain(db, core.DefaultCacheConfigWithScheme(rawdb.HashScheme), nil, nil, ethash.NewFaker(), vm.Config{}, nil, nil)
+
+	return db, bc
+}
+
+// 测试根据地址列表prefetch的运行时间是否变快
+// TODO 能否打印一下各个SLOAD的命中率
+func TestAddrPrefetch(db ethdb.Database, bc *core.BlockChain, block_num uint64, do_prefetch bool) {
+	//读取区块准备环境
+	former_block_num := block_num - 1
+	block_hash := rawdb.ReadCanonicalHash(db, block_num)
+	former_block_hash := rawdb.ReadCanonicalHash(db, former_block_num)
+	block := rawdb.ReadBlock(db, block_hash, block_num)
+	former_block := rawdb.ReadBlock(db, former_block_hash, former_block_num)
+	statedb, _ := bc.StateAt(former_block.Root())
+
+	//如果需要预取则进行相应操作
+	if do_prefetch {
+		fmt.Println(block_num, " do_prefetch")
+		// do former block prefetch
+		prefetch_list := make([][]byte, 0)        //未来我们的工作应该就是维护一个addr prefetch_list
+		for _, tx := range block.Transactions() { //看每个Transaction的To都访问了那些地址
+			prefetch_list = append(prefetch_list, tx.To().Bytes()) //把将要访问的地址存起来
+		}
+		prefetcher := state.NewTriePrefetcher(statedb.GetDB(), statedb.GetOriginalRoot(), "")          //新建一个预取器
+		prefetcher.Prefetch(common.Hash{}, statedb.GetOriginalRoot(), common.Address{}, prefetch_list) //这里只用到了prefetch 指定addr的功能，还可以指定storage slot的
+	} else {
+		fmt.Println(block_num, " without_prefetch")
+	}
+
+	start_time := time.Now()
+	_, _, _, err := bc.Processor().Process(block, statedb, vm.Config{})
+	end_time := time.Now()
+	exec_time := end_time.Sub(start_time).Seconds()
+	fmt.Println("Execute time: ", exec_time, "s")
+	if err != nil {
+		fmt.Println("BC Process err!", err)
+	}
+	fmt.Println()
+}
+
+func main() {
+
+	// TODO看不太出来那个好些，需要打印一下SLOAD命中情况
+	run_cnt := 10
+	for i := 0; i < run_cnt; i++ {
+		fmt.Println("### Run Count", i+1, "###")
+		db, bc := GetBlockChain()
+		TestAddrPrefetch(db, bc, 19795703, true)
+		db.Close()
+		db, bc = GetBlockChain()
+		TestAddrPrefetch(db, bc, 19795703, false)
+		db.Close()
+	}
+}
